09a33a0b2c4be2ab4e7d354aa5fe3719d28b9b22
sionyeom <duatldhs5555@gmail.com>
2022-08-28 22:25:45 +0900

    feat: d

---
+++ app/src/static/js/pw_forgot.js
@@ -0,0 +1,26 @@
+const getPw = () => {
+  // 아이디와 이메일 변수로 저장
+  let id = $("#id").val();
+  let name = $("#email").val();
+  let email = $("#email").val();
+  console.log(name);
+  // 아이디 찾기 axios 함수 작성
+  axios({
+    method: "post",
+    url: "http://localhost:8000/forgot/get_id",
+    data: {
+      // 아이디와 이메일을 보냄
+      id: id,
+      name: name,
+      email: email,
+    },
+  }).then((res) => {
+    if (res.status == 200) {
+      if (res.data.id === undefined) {
+        alert(`입력한 정보가 일치하지 않습니다.`);
+      } else {
+        alert(`회원님의 아이디는 ${res.data.id} 입니다.`);
+      }
+    }
+  });
+};
